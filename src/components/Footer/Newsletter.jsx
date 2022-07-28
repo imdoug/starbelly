466ce858c79c876619fece4e133dc95426c06355
Doug Moreira <dougmoreira@Dougs-MBP-39.lan>
2022-07-27 23:22:15 -0400

    Newsletter done

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import Subheading  from '../SubHeading/SubHeading'
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+      <Subheading title="Newsletter" />
+      <h1 className='headtext__cormorant'>Subscribe To Our Newsletter</h1>
+      <p className='p__opensans'>And Never Miss Latest Updates!</p>
+    </div>
+    <div className='app__newsletter-input flex__center'>
+      <input type="email" placeholder='Enter your e-mail address' />
+      <button className='custom__button'>Subscribe</button>
+
+    </div>
   </div>
 );
 
