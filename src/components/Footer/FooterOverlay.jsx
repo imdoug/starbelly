a6b1c720d93341b778d53b6b0d0c0780b371f334
Doug Moreira <dougmoreira@Dougs-MBP-39.lan>
2022-07-27 23:54:01 -0400

    footer

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import './FooterOverlay.css';
 
 const FooterOverlay = () => (
-  <div className='app__footerOverlay'>
-    <div className='app__footerOverlay-black'></div>
-    <div className='app_footerOverlay-img app__bg'></div>
+  <div className="app__footerOverlay">
+    <div className="app__footerOverlay-black" />
+    <div className="app__footerOverlay-img app__bg" />
   </div>
 );
 
--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,18 +1,19 @@
+
 import React from 'react';
-import Subheading  from '../SubHeading/SubHeading'
+
+import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div className='app__newsletter'>
-    <div className='app__newsletter-heading'>
-      <Subheading title="Newsletter" />
-      <h1 className='headtext__cormorant'>Subscribe To Our Newsletter</h1>
-      <p className='p__opensans'>And Never Miss Latest Updates!</p>
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And never miss latest Updates!</p>
     </div>
-    <div className='app__newsletter-input flex__center'>
-      <input type="email" placeholder='Enter your e-mail address' />
-      <button className='custom__button'>Subscribe</button>
-
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter your email address" />
+      <button type="button" className="custom__button">Subscribe</button>
     </div>
   </div>
 );
